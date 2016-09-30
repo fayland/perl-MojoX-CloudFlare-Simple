98902df3e0b772ed7f695bcf1000f77a4f092177
Fayland Lam <fayland@gmail.com>
2016-09-30 22:27:40 +0800

    Add zone_analytics.pl

--- examples/purge_files.pl
+++ examples/purge_files.pl
@@ -21,12 +21,31 @@ while (my $line = <$fh>) {
     $config->{$x} = $y;
 }
 
+use List::MoreUtils qw/natatime/;
+
+my @files_to_purge = split(/[\r\n]+/, <<'URLS');
+https://bsportsfan.com/
+https://assets.bsportsfan.com/images/team/b/1366.png
+https://assets.bsportsfan.com/images/team/b/154426.png
+https://assets.bsportsfan.com/images/team/b/165358.png
+https://assets.bsportsfan.com/images/team/b/180864.png
+https://assets.bsportsfan.com/images/team/b/193514.png
+https://assets.bsportsfan.com/images/team/b/237474.png
+https://assets.bsportsfan.com/images/team/b/237476.png
+https://assets.bsportsfan.com/images/team/b/237480.png
+https://assets.bsportsfan.com/images/team/b/237482.png
+https://assets.bsportsfan.com/images/team/b/237484.png
+URLS
+
+
 # Purge individual files
 my $cloudflare = MojoX::CloudFlare::Simple->new($config);
-my $result = $cloudflare->request('DELETE', "zones/$config->{zone_id}/purge_cache", {
-    files => [
-        'http://bsportsfan.com/',
-        'https://assets.bsportsfan.com/images/team/s/34953.png'
-    ]
-});
-say Dumper(\$result);
+my $it = natatime 30, @files_to_purge;
+while (my @vals = $it->()) {
+    my $result = $cloudflare->request('DELETE', "zones/$config->{zone_id}/purge_cache", {
+        files => \@vals
+    });
+    say Dumper(\$result);
+
+    sleep 1;
+}
